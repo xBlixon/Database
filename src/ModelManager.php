0edc590df3731cff7ca70a71de03a8d1b864666e
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2023-11-01 20:03:09 +0100

    partially rebuilt ModelManager

--- src/ModelManager.php
+++ src/ModelManager.php
@@ -9,14 +9,12 @@ class ModelManager
 {
     /** @var string|BaseModel $model */
     private string|BaseModel $model;
-    private string $where = "";
-    private string $queryStart;
-    private string $operation;
-    private string $limit = "";
+    private string $query;
+    private int $limit = -1;
 
-    public function __construct(?string $model=NULL)
+    public function __construct(?string $model = NULL)
     {
-        if($model) {
+        if ($model) {
             $this->model = $model;
         }
     }
@@ -26,155 +24,93 @@ class ModelManager
         $this->model = $model;
     }
 
-    private function fetch(): void
+    public function fetch(): self
     {
         $tableName = $this->model::TABLE_NAME;
-        $this->queryStart = "SELECT * FROM $tableName";
-    }
-
-    public function fetchAll(): self
-    {
-        $this->fetch();
-        $this->operation = "fetchAll";
+        $this->query = "SELECT * FROM $tableName";
         return $this;
     }
 
-    public function fetchOne(): self
+    public function where(): self
     {
-        $this->fetch();
-        $this->limit(1);
-        $this->operation = "fetchOne";
+        $this->query .= " WHERE";
         return $this;
     }
 
-    public function whereIs(string $property, mixed $value): self
+    public function and(): self
     {
-        $this->where = "WHERE $property = '$value'";
+        $this->query .= " AND";
         return $this;
     }
 
-    public function andWhereIs(string $property, mixed $value): self
+    public function or(): self
     {
-        $this->where .= " AND $property = '$value'";
+        $this->query .= " OR";
         return $this;
     }
 
-    public function orWhereIs(string $property, mixed $value): self
+    public function equal(string $property, mixed $value): self
     {
-        $this->where .= " OR $property = '$value'";
+        $this->query .= " $property = '$value'";
         return $this;
     }
 
-    public function whereGreater(string $property, mixed $value): self
+    public function greater(string $property, mixed $value): self
     {
-        $this->where = "WHERE $property > '$value'";
+        $this->query .= " $property > '$value'";
         return $this;
     }
 
-    public function andWhereGreater(string $property, mixed $value): self
+    public function greaterOrEqual(string $property, mixed $value): self
     {
-        $this->where .= " AND $property > '$value'";
+        $this->query .= " $property >= '$value'";
         return $this;
     }
 
-    public function orWhereGreater(string $property, mixed $value): self
+    public function lesser(string $property, mixed $value): self
     {
-        $this->where .= " OR $property > '$value'";
+        $this->query .= " $property < '$value'";
         return $this;
     }
 
-    public function whereGreaterOrEqual(string $property, mixed $value): self
+    public function lesserOrEqual(string $property, mixed $value): self
     {
-        $this->where = "WHERE $property >= '$value'";
+        $this->query .= " $property <= '$value'";
         return $this;
     }
 
-    public function andWhereGreaterOrEqual(string $property, mixed $value): self
+    public function between(string $property, mixed $min, mixed $max): self
     {
-        $this->where .= " AND $property >= '$value'";
-        return $this;
-    }
-
-    public function orWhereGreaterOrEqual(string $property, mixed $value): self
-    {
-        $this->where .= " OR $property >= '$value'";
-        return $this;
-    }
-
-    public function whereLesser(string $property, mixed $value): self
-    {
-        $this->where = "WHERE $property < '$value'";
-        return $this;
-    }
-
-    public function andWhereLesser(string $property, mixed $value): self
-    {
-        $this->where .= " AND $property < '$value'";
-        return $this;
-    }
-
-    public function orWhereLesser(string $property, mixed $value): self
-    {
-        $this->where .= " OR $property < '$value'";
-        return $this;
-    }
-
-    public function whereLesserOrEqual(string $property, mixed $value): self
-    {
-        $this->where = "WHERE $property <= '$value'";
-        return $this;
-    }
-
-    public function andWhereLesserOrEqual(string $property, mixed $value): self
-    {
-        $this->where .= " AND $property <= '$value'";
-        return $this;
-    }
-
-    public function orWhereLesserOrEqual(string $property, mixed $value): self
-    {
-        $this->where .= " OR $property <= '$value'";
+        $this->query .= " $property BETWEEN '$min' AND '$max'";
         return $this;
     }
 
     public function like(string $property, mixed $value): self
     {
-        $this->where = "WHERE $property LIKE '$value'";
-        return $this;
-    }
-
-    public function andLike(string $property, mixed $value): self
-    {
-        $this->where .= " AND $property LIKE '$value'";
-        return $this;
-    }
-
-    public function orLike(string $property, mixed $value): self
-    {
-        $this->where .= " OR $property LIKE '$value'";
+        $this->query .= " $property LIKE '$value'";
         return $this;
     }
 
     public function limit(int $limit): self
     {
-        $this->limit = "LIMIT $limit";
+        $this->limit = $limit;
+        $this->query .= " LIMIT $limit";
         return $this;
     }
 
     public function getSQL(): string
     {
-        return "$this->queryStart $this->where $this->limit;";
+        return $this->query;
     }
 
     public function query(): mixed
     {
         $sql = $this->getSQL();
         $queryResult = DatabaseManager::query($sql);
-        return match ($this->operation) {
-            "fetchAll" => $this->manyResultsToModels($queryResult),
-            "fetchOne" => $this->resultToModel($queryResult->fetch()),
-            default => NULL,
-        };
+        if ($this->limit === 1) {
+            return $this->resultToModel($queryResult->fetch());
+        }
+        return $this->manyResultsToModels($queryResult);
     }
 
     public function save(BaseModel $model): mixed
@@ -182,8 +118,7 @@ class ModelManager
         $tableName = $this->model::TABLE_NAME;
         $modelReflection = new ReflectionClass($model);
         $id = $model->getId();
-        if($id !== -1)
-        {
+        if ($id !== -1) {
             $updateString = "";
             foreach ($modelReflection->getProperties() as $property) {
                 $value = ($property->isInitialized($model) ? $property->getValue($model) : "NULL");
@@ -191,9 +126,7 @@ class ModelManager
             }
             $updateString = rtrim($updateString, ", ");
             $sql = "UPDATE $tableName SET $updateString WHERE id = '$id'";
-        }
-        else
-        {
+        } else {
             $columns = implode(", ", array_keys(DatabaseManager::getModelColumns($model)));
             $insertString = "";
             foreach ($modelReflection->getProperties() as $property) {
@@ -225,7 +158,7 @@ class ModelManager
         (new ReflectionProperty(BaseModel::class, 'id'))->setValue($modelInstance, $sqlModel['id']);
         unset($sqlModel['id']);
         foreach ($sqlModel as $property => $value) {
-            if(is_int($property)) {
+            if (is_int($property)) {
                 continue;
             }
             (new ReflectionProperty($modelInstance, $property))->setValue($modelInstance, $value);
